ba612cafefa7af05aea26691e85ed1ec61182353
Gabriel Valle <gvalle91@gmail.com>
2018-07-01 15:20:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,11 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each {|item| resp.write "#{item}\n"}
+      if @@cart.size == 0
+        resp.write "Your cart is empty"
+      else
+        @@cart.each {|item| resp.write "#{item}\n"}
+      end
     elsif req.path.match(/add/)
       add_item = req.params["item"]
       if @@items.include?(add_item)
